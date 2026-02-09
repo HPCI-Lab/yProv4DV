484984656cfc4cdb2288619c4ba4507d889718f4
Mona Lisa <gabriele.padovani@gmail.com>
2026-02-09 13:47:14 -0500

    added automatic data subset collection

--- examples/simple.py
+++ examples/simple.py
@@ -1,16 +1,24 @@
+import sys
+sys.path.append(".")
 import yprov4dv
+yprov4dv.start_run(create_rocrate=False, create_json_file=True, create_dot_file=True, create_svg_file=True)
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from lib import elaborate
 
 data = pd.read_csv("./assets/results.csv")
-yprov4dv.log_input("./assets/results.csv")
+# yprov4dv.log_input("./assets/results.csv")
 
 data["second_series"] = elaborate(data["points"])
 
-data.plot()
-plt.legend()
-plt.savefig("tmp.png")
+# plt.plot(data["points"], data["second_series"])
+# plt.legend()
+# plt.savefig("tmp1.png")
 
-yprov4dv.log_output("tmp.png")
+sns.lineplot(data,x="points", y="second_series")
+plt.legend()
+plt.savefig("tmp2.png")
+
+# yprov4dv.log_output("tmp.png")
--- yprov4dv/yprov4dv.py
+++ yprov4dv/yprov4dv.py
@@ -6,23 +6,74 @@ from prov.model import ProvDocument
 from pathlib import Path
 import shutil
 import shlex
-import warnings
 import pandas as pd
 import xarray as xr
 import geopandas as gpd
 import rasterio as rio
 import numpy as np
 import torch
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from . import utils
 from . import file_utils
 
 class ProvTracker:
-    def __init__(self):
-        self.PREFIX = os.environ.get('YPROV4DV_PREFIX', "yProv4DA")
+    def _track_read_calls(self): 
+        pd.read_csv = self._track_read_path(pd.read_csv)
+        pd.read_parquet = self._track_read_path(pd.read_parquet)
+        pd.read_excel = self._track_read_path(pd.read_excel)
+        pd.read_json = self._track_read_path(pd.read_json)
+        xr.open_dataset = self._track_read_path(xr.open_dataset)
+        xr.open_mfdataset = self._track_read_path(xr.open_mfdataset)
+        gpd.read_file = self._track_read_path(gpd.read_file)
+        rio.open = self._track_read_path(rio.open)
+        np.load = self._track_read_path(np.load)
+        torch.load = self._track_read_path(torch.load)
+
+    def _track_plot_calls(self): 
+        self._orig_plot = plt.plot
+        plt.plot = self._wrapped_plt_plot
+
+        self._orig_pandas_call = pd.plotting._core.PlotAccessor.__call__
+        tracker_instance = self
+        def pandas_stub(accessor, *args, **kwargs):
+            data = accessor._parent 
+            tracker_instance._wrapped_pd_plot(data)
+            return tracker_instance._orig_pandas_call(accessor, *args, **kwargs)
+        pd.plotting._core.PlotAccessor.__call__ = pandas_stub
+
+        sns_plots = ['scatterplot', 'lineplot', 'barplot', 'histplot', 'boxplot']
+        for func_name in sns_plots:
+            orig_func = getattr(sns, func_name)
+            setattr(sns, func_name, self._make_sns_wrapper(orig_func))
         
+    def __init__(
+            self, 
+            run_name : str = "experiment_run", 
+            provenance_directory : str = "prov", 
+            prefix : str = "yProv4DA", 
+            default_namespace : str = "http://example.org/", 
+            create_json_file : bool = False, 
+            create_dot_file : bool = False, 
+            create_svg_file : bool = False, 
+            create_rocrate : bool = True,
+            save_input_files_full : bool = True, 
+            save_input_files_subset : bool = False,
+            verbose : bool = False, 
+        ): 
+
+        self.accessed_files = {}
+        self.plot_count = 0
+
+        self.PREFIX = prefix
         self.RUN_ID = 0
-        self.EXPERIMENT_DIR = f"{os.environ.get('YPROV4DV_PROVENANCE_DIRECTORY', "prov")}_{self.RUN_ID}"
+        self.EXPERIMENT_DIR = f"{provenance_directory}_{self.RUN_ID}"
+        
+        self.doc = ProvDocument()
+        self.doc.set_default_namespace(default_namespace)
+        self.doc.add_namespace(self.PREFIX, self.PREFIX)
+
         if os.path.exists(self.EXPERIMENT_DIR):
             prev_exps = os.listdir(".") 
             experiment_name = self.EXPERIMENT_DIR.removesuffix("_0")
@@ -36,46 +87,42 @@ class ProvTracker:
         self.OUTPUTS_DIR = os.path.join(self.EXPERIMENT_DIR, "outputs")
         self.PROJECT_ROOT = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-        self.RUN_NAME = os.environ.get('YPROV4DV_RUN_NAME', "experiment_run")
-        self.verbose = utils.parse_bool(os.environ.get('YPROV4DV_VERBOSE', "False"))
-        self.create_json = utils.parse_bool(os.environ.get('YPROV4DV_CREATE_JSON_FILE', "False"))
-        self.create_graph = utils.parse_bool(os.environ.get('YPROV4DV_CREATE_DOT_FILE', "False"))
+        if not os.path.exists(self.INPUTS_DIR):
+            os.makedirs(self.INPUTS_DIR, exist_ok=True)
+        if not os.path.exists(self.OUTPUTS_DIR):
+            os.makedirs(self.OUTPUTS_DIR, exist_ok=True)
+        if not os.path.exists(self.SRC_DIR):
+            os.makedirs(self.SRC_DIR, exist_ok=True)
+
+        self.RUN_NAME = run_name
+        self.verbose = verbose
+        self.create_json = create_json_file
+        self.create_graph = create_dot_file
         if self.create_graph and not self.create_json: 
             self.create_json = True
             if self.verbose: 
-                warnings.warn("[ProvTracker] YPROV4DV_CREATE_JSON_FILE cannot be False when requesting YPROV4DV_CREATE_DOT_FILE, turning it to True")
-        self.create_svg = utils.parse_bool(os.environ.get('YPROV4DV_CREATE_SVG_FILE', "False"))
+                print("[ProvTracker] YPROV4DV_CREATE_JSON_FILE cannot be False when requesting YPROV4DV_CREATE_DOT_FILE, turning it to True")
+        self.create_svg = create_svg_file
         if self.create_svg and not (self.create_json and self.create_graph): 
             self.create_json, self.create_graph = True, True
             if self.verbose: 
-                warnings.warn("[ProvTracker] YPROV4DV_CREATE_JSON_FILE and YPROV4DV_CREATE_DOT_FILE cannot be False when requesting YPROV4DV_CREATE_SVG_FILE, turning them to True")
+                print("[ProvTracker] YPROV4DV_CREATE_JSON_FILE and YPROV4DV_CREATE_DOT_FILE cannot be False when requesting YPROV4DV_CREATE_SVG_FILE, turning them to True")
+        self.crate_ro_crate = create_rocrate
 
-        self.crate_ro_crate = utils.parse_bool(os.environ.get('YPROV4DV_CREATE_RO_CRATE', "True"))
+        self.save_input_files_full = save_input_files_full
+        if self.save_input_files_full: 
+            self._track_read_calls()
+        self.save_input_files_subset = save_input_files_subset
+        if self.save_input_files_subset: 
+            self._track_plot_calls()
 
-        self.accessed_files = {}
-
-        self.doc = ProvDocument()
-        self.doc.set_default_namespace(os.environ.get('YPROV4DV_DEFAULT_NAMESPACE', 'http://example.org/'))
-        self.doc.add_namespace(self.PREFIX, self.PREFIX)
         self.start_time = time.time()
-
         self.start_snapshot = file_utils.snapshot(".")
 
-        pd.read_csv = self.track_path(pd.read_csv)
-        pd.read_parquet = self.track_path(pd.read_parquet)
-        pd.read_excel = self.track_path(pd.read_excel)
-        pd.read_json = self.track_path(pd.read_json)
-        xr.open_dataset = self.track_path(xr.open_dataset)
-        xr.open_mfdataset = self.track_path(xr.open_mfdataset)
-        gpd.read_file = self.track_path(gpd.read_file)
-        rio.open = self.track_path(rio.open)
-        np.load = self.track_path(np.load)
-        torch.load = self.track_path(torch.load)
-
         if self.verbose: 
-            warnings.warn("[ProvTracker] Monitoring started...")
+            print("[ProvTracker] Monitoring started...")
 
-    def track_path(self, func):
+    def _track_read_path(self, func):
         def wrapper(path, *a, **kw):
             try:
                 self.accessed_files[Path(path).resolve()] = "r"
@@ -83,18 +130,75 @@ class ProvTracker:
                 pass  # file-like objects, buffers, URLs, etc.
             return func(path, *a, **kw)
         return wrapper
+    
+    
+    def _make_sns_wrapper(self, orig_func):
+        def wrapper(*args, **kwargs):
+            # Seaborn usually takes data via the 'data' keyword argument
+            data = kwargs.get('data')
+            x = kwargs.get('x')
+            y = kwargs.get('y')
+            if data is None and x is None and y is None: 
+                data = args[0]
+            if x is not None and isinstance(x, str): 
+                data = args[0]
+                xdata = data[x]
+            if y is not None and isinstance(y, str): 
+                data = args[0]
+                ydata = data[y]
+            if data is not None:
+                self._wrapped_pd_plot(data)
+            if xdata is not None and ydata is not None: 
+                self._wrapped_plt_plot(xdata, ydata)
+            return orig_func(*args, **kwargs)
+        return wrapper
+    
+    def _wrapped_pd_plot(self, *args, **kwargs): 
+        self.plot_count += 1
+        if self.verbose: 
+            print(f"[ProvTracker] Tracking pandas or seaborn plot...")
+
+        x_data = args[0]            
+        if isinstance(x_data, (pd.DataFrame, pd.Series, np.ndarray)):
+            xfilename = os.path.join(self.INPUTS_DIR, f"data_{self.plot_count}.csv")
+            pd.DataFrame(x_data).to_csv(xfilename)
+            self.accessed_files[Path(xfilename).resolve()] = "r"
+
+        return self._orig_plot(*args, **kwargs)
+
+    def _wrapped_plt_plot(self, *args, **kwargs):
+        self.plot_count += 1
+        if self.verbose: 
+            print(f"[ProvTracker] Tracking matplotlib plot...")
+
+        print(len(args))
+        if len(args) >= 2:
+            x_data = args[0]            
+            if isinstance(x_data, (pd.Series, np.ndarray)):
+                xfilename = os.path.join(self.INPUTS_DIR, f"xdata_{self.plot_count}.csv")
+                pd.DataFrame(x_data).to_csv(xfilename)
+                self.accessed_files[Path(xfilename).resolve()] = "r"
+
+            y_data = args[1]
+            if isinstance(y_data, (pd.Series, np.ndarray)):
+                yfilename = os.path.join(self.INPUTS_DIR, f"ydata_{self.plot_count}.csv")
+                pd.DataFrame(y_data).to_csv(yfilename)
+                self.accessed_files[Path(yfilename).resolve()] = "r"
+
+        return self._orig_plot(*args, **kwargs)
 
     def copy_file_to(self, file, _dir): 
         if self.verbose: 
-            warnings.warn(f"[ProvTracker] Copy File {file} to {_dir}...")
+            print(f"[ProvTracker] Copy File {file} to {_dir}...")
         filename = Path(file).name
         file_dst = os.path.join(_dir, filename)
-        shutil.copyfile(file, file_dst)
+        if file != Path(file_dst).resolve(): 
+            shutil.copyfile(file, file_dst)
         return file_dst
 
     def finalize(self):
         if self.verbose: 
-            warnings.warn("[ProvTracker] Script ending. Analyzing changes...")
+            print("[ProvTracker] Script ending. Analyzing changes...")
 
         end_snapshot = file_utils.snapshot(".")
         created = end_snapshot.keys() - self.start_snapshot.keys()
@@ -105,13 +209,6 @@ class ProvTracker:
             log_output(m)
 
         activity = self.doc.activity(f'{self.PREFIX}:{self.RUN_NAME}', time.ctime(self.start_time), time.ctime())
-
-        if not os.path.exists(self.INPUTS_DIR):
-            os.makedirs(self.INPUTS_DIR, exist_ok=True)
-        if not os.path.exists(self.OUTPUTS_DIR):
-            os.makedirs(self.OUTPUTS_DIR, exist_ok=True)
-        if not os.path.exists(self.SRC_DIR):
-            os.makedirs(self.SRC_DIR, exist_ok=True)
 
         repo = file_utils._get_git_remote_url()
         if repo is not None:
@@ -126,7 +223,7 @@ class ProvTracker:
             if  "r" in perm: 
                 file_dst = self.copy_file_to(file, self.INPUTS_DIR)
                 entity = self.doc.entity(f'{self.PREFIX}:{file_dst}')
-                self.doc.used(entity, activity)
+                self.doc.used(activity, entity)
             elif "w" in perm: 
                 file_dst = self.copy_file_to(file, self.OUTPUTS_DIR)
                 entity = self.doc.entity(f'{self.PREFIX}:{file_dst}')
@@ -136,7 +233,7 @@ class ProvTracker:
         for file in file_sources: 
             file_dst = self.copy_file_to(file, self.SRC_DIR)
             entity = self.doc.entity(f'{self.PREFIX}:{file_dst}')
-            self.doc.used(entity, activity)
+            self.doc.used(activity, entity)
 
         output_file = f'{self.RUN_NAME}.json'
         path_json = os.path.join(self.EXPERIMENT_DIR, output_file)
@@ -144,20 +241,36 @@ class ProvTracker:
             with open(path_json, 'w') as f:
                 f.write(self.doc.serialize())
             if self.verbose: 
-                warnings.warn(f"[ProvTracker] Provenance saved to {path_json}")
+                print(f"[ProvTracker] Provenance saved to {path_json}")
 
         if self.create_graph: 
             path_graph = os.path.join(self.EXPERIMENT_DIR, output_file)
             utils.save_prov_file(self.doc, self.EXPERIMENT_DIR, path_graph, self.create_graph, self.create_svg)
             if self.verbose: 
-                warnings.warn(f"[ProvTracker] Provenance graph to {path_graph}")
+                print(f"[ProvTracker] Provenance graph to {path_graph}")
 
         if self.crate_ro_crate: 
             file_utils.create_rocrate_in_dir(self.EXPERIMENT_DIR)
 
-_instance = ProvTracker()
-atexit.register(_instance.finalize)
+_instance = None
     
+def start_run(
+    run_name : str = "experiment_run", 
+    provenance_directory : str = "prov", 
+    prefix : str = "yProv4DA", 
+    default_namespace : str = "http://example.org/", 
+    create_json_file : bool = False, 
+    create_dot_file : bool = False, 
+    create_svg_file : bool = False, 
+    create_rocrate : bool = True,
+    save_input_files_full : bool = True, 
+    save_input_files_subset : bool = True,
+    verbose : bool = False, 
+    ): 
+    global _instance
+    _instance = ProvTracker(run_name, provenance_directory, prefix, default_namespace, create_json_file, create_dot_file, create_svg_file, create_rocrate, save_input_files_full, save_input_files_subset, verbose)
+    atexit.register(_instance.finalize)
+
 def log_input(path): 
     log_file(path, "r")
 
@@ -170,6 +283,6 @@ def log_file(path, mode):
     for p in _instance.accessed_files.keys(): 
         if p.name in path.name or path.name in p.name: 
             if utils.paths_are_same(p, path): 
-                warnings.warn(f"[ProvTracker] Attempt to log {path} when {p} has already been logged with mode \"{mode}\"")
+                print(f"[ProvTracker] Attempt to log {path} when {p} has already been logged with mode \"{mode}\"")
                 return
     _instance.accessed_files[path] = mode
